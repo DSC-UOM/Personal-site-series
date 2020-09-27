160f202f3455833cdc677992640e452d6589529a
Gurneet Bhatia <sbgurneet@gmail.com>
2020-09-27 20:17:14 +0100

    feat: injected data service into xps

--- src/app/pages/experiences/experiences.component.ts
+++ src/app/pages/experiences/experiences.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+import { DataService } from 'src/app/services/data.service';
+import { ExperienceModel } from 'src/app/shared/models/experience';
+
 @Component({
   selector: 'app-experiences',
   templateUrl: './experiences.component.html',
@@ -7,9 +10,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ExperiencesComponent implements OnInit {
 
-  constructor() { }
+  experiences: ExperienceModel[];
+
+  constructor(private dataService: DataService) { }
 
   ngOnInit(): void {
+    this.experiences = this.dataService.getExperiences();
   }
 
 }

66673dbc7094397b8fec5a2030f47b3ab80b6fd2
Gurneet Bhatia <sbgurneet@gmail.com>
2020-09-27 20:13:21 +0100

    feat: connected up fake data obj to device service

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
 
+import { experiences } from '../shared/data';
+import { ExperienceModel } from '../shared/models/experience';
+
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
 
   constructor() { }
+
+  getExperiences(): ExperienceModel[] {
+    return experiences;
+  }
 }

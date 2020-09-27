ae4e4e6d121b5858b6cb827a51b6ab4a4f4e3315
Gurneet Bhatia <sbgurneet@gmail.com>
2020-09-27 18:35:47 +0100

    feat: basic design of the navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatChipsModule } from '@angular/material/chips';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -18,6 +20,8 @@ import { NavbarComponent } from './shared/navbar/navbar.component';
     BrowserModule,
     BrowserAnimationsModule,
     MatChipsModule,
+    MatToolbarModule,
+    MatButtonModule,
     AppRoutingModule
   ],
   providers: [],
